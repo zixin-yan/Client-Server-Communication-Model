db6013831a538fe230a02cd7fc98907b4df5a04a
zixin-yan <yan.zix@northeastern.edu>
2022-04-05 01:50:53 -0400

    Modified self.clients

--- server/chat_server.py
+++ server/chat_server.py
@@ -5,8 +5,9 @@ from chat_message import ChatMessage
 
 class ChatServer:
     def __init__(self, host='127.0.0.1', port=1234, buffer_size=1024):
-        self.host = host # the server's host name or IP address
-        self.port = port # the port used by the server
+        self.clients = []
+        self.host = host  # the server's host name or IP address
+        self.port = port  # the port used by the server
         self.buffer_size = buffer_size
 
     def start(self):
@@ -24,7 +25,7 @@ class ChatServer:
                 username = username.decode('utf-8')
                 text = f'{username} has joined the chat'
                 print(text)
-                self.boardcast_massage(text)
+                self.broadcast_message(text)
 
                 self.clients.append(conn)
 
@@ -41,13 +42,13 @@ class ChatServer:
                 print(f'Received message: {message.text}')
 
                 text = f'{username} said: {message.text}'
-                self.boardcast_message(text)
+                self.broadcast_message(text)
             self.clients.remove(conn)
             text = f'{username} has disconnected from the chat'
             print(text)
-            self.boardcast_message(text)
+            self.broadcast_message(text)
 
-    def boardcast_message(self, text):
+    def broadcast_message(self, text):
         message = ChatMessage(text)
         message_binary = pickle.dumps(message)
         for client in self.clients:
